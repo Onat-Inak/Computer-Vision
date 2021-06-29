9e72b11888de57e6326c630492fcebefc6fa59f0
Moritz-hash <85705292+Moritz-hash@users.noreply.github.com>
2021-06-29 13:39:33 +0200

    Function to change the reference image
    This is the first version of the discussed function with which you can change the reference image. Please read the documentation at the beginning.

---
+++ Change_ref_im.m
@@ -0,0 +1,60 @@
+function [trafos_new,imstruct_new]=Change_ref_im(trafos,im_struct,ind_old_ref,ind_new_ref)
+%This function changes the referencimage and the aligned images as well as the necessary
+%transformations. Please read the instruction.
+
+%inputs:
+    %trafos: transformation matrix
+    %im_struct: original images (not transformed images!!! Otherwise you would end up with nonsense (allready transformed))
+    %ind_old_ref: index of the old reference image
+    %ind_new_ref: index of new reference image, this must be a linked image (i.e.: trafos{ind_old_ref,ind_new_ref} must contain a valid trafo)
+    
+
+%output:
+    %trafos_new: transformations to the orientation of the new reference image
+    %imstruct_new: transformed images wrt. the new reference image
+    
+    
+%basic idea is using the chain of transformations: im1->im_ref_new =
+%im1->im_ref_old->im_ref_new, whereas the last transformation is just the
+%inverse transformation of im_ref_new->im_ref_old. These transformations
+%can be found in 'trafos'.
+
+if isstring(trafos{ind_old_ref,ind_new_ref}) %the caller tries to transfor ont a not linked reference image-> throw error
+    error('You are trying to convert to a not linked image, please consider using a different new reference image via ind_new_ref');
+end
+
+%initialize:
+trafos_new=cell(size(trafos));
+imstruct_new=cell(size(im_struct));
+
+%for easier working: set element in trafos at position ind_old_ref to I (no
+%case destinction later)
+
+trafos{ind_old_ref,ind_old_ref}=projective2d(eye(3));
+
+
+%going over all images that could be linked:
+for i=1:numel(im_struct)
+    
+    if ~isstring(trafos{ind_old_ref,i}) %check if linked (otherwise it contains a string)
+    %calculate new trafo over above described chain:
+    trafos_new{ind_new_ref,i}=projective2d((trafos{ind_old_ref,i}.T)/(trafos{ind_old_ref,ind_new_ref}.T));
+    
+    
+    %apply trafo to image:
+    imstruct_new{i}= apply_transformation(im_struct{ind_old_ref},im_struct{i},trafos_new{ind_new_ref,i});
+
+    
+    else %no trafo was found bevor -> not possoble to link-> insert "not Working" with a capital 'W' to maintain original structure
+        trafos_new{ind_new_ref,i}='not Working'; %emphasis on the capital 'W'
+    end
+    
+end
+
+
+%Further explanation: https://www.youtube.com/watch?v=dQw4w9WgXcQ
+    
+    
+
+end
+
