b8b20215005478f83ed54cb396dad8ce56b96882
Moritz-hash <85705292+Moritz-hash@users.noreply.github.com>
2021-07-04 23:33:22 +0200

    Update Diffence_Magnitude for filtering by size
    With this update, the Difference_Magnitude function is extended by the optional filtering of differences by region size, they appear in.

--- Difference_Magnitude.m
+++ Difference_Magnitude.m
@@ -1,4 +1,4 @@
-function [Image_Marked,Diff_image_threshold] = Difference_Magnitude(Image_ref,Image_move,change_threshold,plot_Images,seg_flag)
+function [Image_Marked,Diff_image_threshold] = Difference_Magnitude(Image_ref,Image_move,change_threshold,plot_Images,seg_flag,l_filter)
 % Moritz Schneider, Adam Misik, Onat Inak, Robert Jacumet
 % Computer Vision Project SS21, Group 30
 %% Get and show Boundaries of reconstructed Moving Image
@@ -112,6 +112,54 @@ elseif plot_Images
     imshow(Image_Marked);
     title("Reconstructed Image with marked changes");
 end
+
+
+
+%% Differentiation of sizes of region of change
+% This part allows for a differentiation between sizes of image changes.
+% Changes that lie in image regions with only few changes can be filtered
+% out, leaving changes that lie in regions with a lot change.
+% It filters the mask obtained from the difference image with a lowpass
+% filter, filtering out smaller regions of changes. This is a faster, but
+% not so precise, alternative to the super pixels approach.
+%
+% Aspects: this is for comparing two images given the sliders for
+% change_threshold and l_filter.
+% The change_threshold is the minimal magnitude of change (0-100%) considered, with
+% the best results usually being in found between 20% and 50%.
+% The length of filter l_filter is propotional to the minimum size of change depicted and
+% values from 3-200 are reasonable with the best results between 11 and 155.
+
+
+if nargin==nargin(@Difference_Magnitude) %l_filter is given-> use function
+    if l_filter>1 %condition to actually filter out smaller changes
+        l_filter=l_filter-mod(l_filter,2)+1; %set to a valid filter length, just in case.
+        
+        diff_size_mask=Diff_image_threshold;
+        diff_size_mask(diff_size_mask>0)=1;%set to one instead of 255 for better calculation
+        %initialize guassian filter:
+        sig=floor(l_filter/2)/2;
+        x=-floor(l_filter/2):floor(l_filter/2);
+        G=exp(-(x.^2)/(2*sig^2));
+        G=G/sum(G);%normalize filter
+        %apply filter:
+        diff_size_mask=double(diff_size_mask);
+        diff_size_mask=conv2(diff_size_mask,G,'same');
+        diff_size_mask=conv2(diff_size_mask,G','same');
+        diff_size_mask(abs(diff_size_mask)<0.15)=0;
+        diff_size_mask(abs(diff_size_mask)>0)=1; %again set to 1 for better calculation in code below
+        %This mask masks out the big regions of change.
+        %now combine diff_size_mask and original difference image, to obtain the detailed differences in big regions of change:
+        Diff_image_threshold=Diff_image_threshold.*uint8(diff_size_mask);
+        %mark it in image:
+        Image_Marked=Image_ref;%it might be better to show changes in the new image
+        Image_Marked_red_channel=Image_Marked(:,:,1);
+        Image_Marked_red_channel(Diff_image_threshold>0)=255;
+        Image_Marked(:,:,1)=Image_Marked_red_channel;
+        
+    end
+end
+
 
 end %end of function
 
