24144b2b81e887d7715e2989cc467cae93b331c7
Moritz-hash <85705292+Moritz-hash@users.noreply.github.com>
2021-07-09 18:44:11 +0200

    Update export_reconstructed_imgs.m

--- export_reconstructed_imgs.m
+++ export_reconstructed_imgs.m
@@ -1,12 +1,13 @@
 function export_reconstructed_imgs(Images_reconstructed, fileNames_processed,folderName_processed)
-%Code zum exportieren der normalisierten Bilder
-%benötigt: folderName_processed, fileNames_processed, so wie
-%Images_reconstructed aus der GUI
+%Code used to export the normalized images
+%required inputs: folderName_processed, fileNames_processed, as well as
+%Images_reconstructed from the GUI
 
-mkdir(strcat('Exported_Imgs/',folderName_processed))
-for ever=1:numel(fileNames_processed)
-filename=strcat('Exported_Imgs/',folderName_processed,'/',fileNames_processed{ever});
-imwrite(Images_reconstructed{ever}, filename);
+mkdir(strcat('Exported_Imgs/',folderName_processed)) %create directory
+
+for ever=1:numel(fileNames_processed)%go throug all reconstructed images
+filename=strcat('Exported_Imgs/',folderName_processed,'/',fileNames_processed{ever});%generate filename
+imwrite(Images_reconstructed{ever}, filename);%save image
 end
 
 end
